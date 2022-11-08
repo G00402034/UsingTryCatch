b70a4aae4698add70db6763db6bc84a95373a219
G00402034 <G00402034@atu.ie>
2022-11-08 13:48:52 +0000

    added try catch exeption

--- src/main/java/ie/atu/UsingTryCatch.java
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -1,17 +1,32 @@
 package ie.atu;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class UsingTryCatch {
 
-    public static void main(String[] args)
+    public static void main(String[] args){
+    try
+
     {
         System.out.println("Please enter a number between 0 - 9:");
         Scanner myscan = new Scanner(System.in);
         int myNum = myscan.nextInt();
-        if(myNum <= 9 && myNum > 0){
+        if(myNum <= 9 && myNum > 0) {
             System.out.println("You entered " + myNum);
-        } else {
+        }
+        else {
             System.out.println("Not a valid number");
         }
+
+        }
+
+
+      catch(InputMismatchException e)
+
+    {
+        System.out.println("Error caught");
+
     }
+    }
+
 }
 
